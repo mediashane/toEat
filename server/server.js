b6fce3d6ce1af236584f81bde779ef349c91b7c5
Shane Smith <shane@mediashane.com>
2018-05-19 16:03:01 +0800

    add GET /toeats id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { toEat } = require('./models/eats');
@@ -27,6 +28,23 @@ app.get('/toeats', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+})
+
+app.get('/toeats/:id', (req, res) =>  {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    toEat.findById(id).then((eat) => { 
+        if (!eat) {
+          return res.status(404).send();
+        }
+        res.send({eat})
+    }).catch((e) => console.log(e));
+
+    res.send(req.params);
 })
 
 app.listen(3000, () => {
